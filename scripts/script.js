991bb24fcff21015339bddc78f17ab3534a0b9ae
MariuszPe <mariusz.pela1@gmail.com>
2018-08-09 23:11:01 +0200

    Bugs to fix - saving song when changing screen to playlist & going back, picking song from playlist and trying to go to next/prev song,  progres bar not working.

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,132 @@
-$(function(){
-  $('#container').html(
-    "<div class='rectangle'>" + 
-      "<div class='rectangle__background'>" + 
+$(function () {
+  playListView();
+  events();
+  defaultSongSetting(0);
+});
 
-        "<img src='./img/Background_iconaPop.png'>" + 
-      "</div>" +
-      "<div class='rectangle__background--Top'> <i class='fas fa-redo'></i></div>" +
-    "</div>");
-    
- 
-}); 
+
+
+function events() { 
+    $('#play').click(function () {
+      
+    if($('#play').hasClass('fa-play')) {
+      $('#play').removeClass('fa-play').addClass('fa-pause');
+    } else {
+      $('#play').removeClass('fa-pause').addClass('fa-play');
+    }
+  });
+
+
+  $('#next').click(function () {
+    let actualListNumber = parseInt($('div .playerMiddle').attr('actualListNumber'));
+    if(actualListNumber < playListJSON.playlist.length-1) {
+      defaultSongSetting(actualListNumber+1);
+    }
+  });
+
+
+  $('#prev').click(function () {
+    let actualListNumber = parseInt($('div .playerMiddle').attr('actualListNumber'));
+    if (actualListNumber > 0) {
+      defaultSongSetting(actualListNumber-1);
+    }
+  });
+
+
+  $('li').click(function() {
+
+    let selectedSong = $(this).children().children().html();
+    let length = selectedSong.slice(selectedSong.indexOf('top')+5, selectedSong.indexOf('top')+9);
+    let author = selectedSong.slice(selectedSong.indexOf('| ')+1, selectedSong.indexOf('<br>'));
+    let song = selectedSong.slice(selectedSong.indexOf('bottom')+8, selectedSong.length);
+    mainView();
+    $( "#navigateToMainView" ).click();
+    $('.playerMiddle').html(author + "<br> <span class='songTitle'>" + song + "</span>");
+  });
+}
+
+
+
+function defaultSongSetting(actualListNumber) {
+  $('.playerMiddle').html(playListJSON.playlist[actualListNumber].author + "<br>" +
+    "<span class='songTitle'>" + playListJSON.playlist[actualListNumber].songName + "</span>");
+
+  $('div .playerMiddle').attr('actualListNumber', actualListNumber);
+   
+}
+
+
+function playListView() {
+  
+  $('#navigateToPlaylist').click(function () {
+    let listHtml = [];
+    $.each(playListJSON.playlist, function (i, song) {
+      listHtml.push(
+        "<li><div class='rectangle__playlist--Li'>" +
+          "<div class='rectangle__playlist--LiLeft'>" +
+            "<span class='LiLeft--top'>" + song.length + " | " + song.author + "</span> <br>" +
+            "<span class='LiLeft--bottom'>" + song.songName + "</span>" +
+          "</div>" +
+          "<div class='rectangle__playlist--LiRight'>" +
+            "<i class='fas fa-share-alt'></i><i class='fas fa-heart'></i>" +
+          "</div><br><br><hr>" +
+        "</div></li>");
+    });
+
+
+    $('#container').html(
+      "<div class = 'rectangle'>" +
+        "<div class = 'rectangle__background--Top playlistTop'>" +
+          "<div class = 'topLeft'>" +
+            "<i id='navigateToMainView' class='fas fa-reply'></i>" +
+          "</div>" +
+          "<div class = 'topCenter'> Playlist </div>" +
+        "</div>" +
+        "<div class = 'rectangle__playlist'><ul data-simplebar>" + listHtml.join("") + 
+          "<li><i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i></li></ul></div>" +
+        "<div class='rectangle__playlist--blurry'></div>" +
+      "</div>");
+    events();
+    mainView();
+  });
+}
+
+
+
+function mainView() {
+  $('#navigateToMainView').click(function () {
+
+    $('#container').html(
+      "<div class='rectangle rectangle__backgroundPlaylist'>" +
+        "<div class='rectangle__background--Top playerTop'>" +
+          "<i class='fas fa-retweet'></i>" +
+          "<i class='fas fa-random'></i>" +
+          "<i class='fas fa-redo'></i>" +
+          "<i id='navigateToPlaylist' class='fas fa-bars'></i>" +
+        "</div>" +
+        "<div class='rectangle__background--Top playerMiddle'>" +
+        "</div>" +
+        "<div class='rectangle__background--progresBar'>" +
+          "<progress id='progresBar' max='100' value='80'></progress>" +
+        "</div>" +
+        "<div class='rectangle__background--playerControl'>" +
+          "<i class='fas fa-share-alt'></i>" +
+          "<i id='prev' class='fas fa-step-backward'></i>" +
+          "<i id='play' class='fas fa-play'></i>" +
+          "<i id='next' class='fas fa-step-forward'></i>" +
+          "<i class='fas fa-heart'></i>" +
+        "</div>" +
+      "</div>");
+
+    if($('.playerMiddle').html() == "") { 
+      defaultSongSetting(0);
+    }
+
+    events();
+    playListView();
+  });
+
+
+
+}
+
---
+++ scripts/songlistJS.js
@@ -0,0 +1,59 @@
+var playListJSON = {
+    "playlist": [
+        {
+            "author": "Icona Pop",
+            "songName": "Still Don't Know",
+            "length": "3:16"
+        },
+        {
+            "author": "Icona Pop",
+            "songName": "I love it",
+            "length": "2:35"
+        },
+        {
+            "author": "Icona Pop",
+            "songName": "Girlfriend",
+            "length": "2:50"
+        },
+        {
+            "author": "Icona Pop",
+            "songName": "We Got the World",
+            "length": "3:07"
+        },
+        {
+            "author": "Icona Pop",
+            "songName": "Night Like This",
+            "length": "3:24"
+        },
+        {
+            "author": "Icona Pop",
+            "songName": "All night",
+            "length": "5:53"
+        },
+        {
+            "author": "Icona Pop",
+            "songName": "Emergency",
+            "length": "2:57"
+        },
+        {
+            "author": "Icona Pop",
+            "songName": "Someone Who Can Dance",
+            "length": "2:52"
+        },
+        {
+            "author": "Icona Pop",
+            "songName": "Then We Kiss",
+            "length": "2:18"
+        },
+        {
+            "author": "Icona Pop",
+            "songName": "Brightside",
+            "length": "2:59"
+        },
+        {
+            "author": "Icona Pop",
+            "songName": "Not Too Young",
+            "length": "3:33"
+        },
+    ]
+}
